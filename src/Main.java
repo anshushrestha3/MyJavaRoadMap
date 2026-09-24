abfe2250e43b61fb421ff6f296fb612a50058dff
anshushrestha3 <anshushrestha06.edu@gmail.com>
2026-09-25 00:20:55 +0545

    i have learned about comments, variables, naming convention, datatype in java, method/function naming convention

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main{
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args)
+    {
+        // 1.comments
+
+        // single line comment
+        // i am a girl
+        // i am not a boy
+
+
+
+        /* multi line comment */
+        /* my name is ambika shrestha
+           i am not a boy
+         */
+
+
+        // 2.variables
+         String newar = " Shrestha, maharjhan, shakhya,"; // newar is variable
+
+
+        String school = " Cct"; // scl is variable
+
+        String[] scl = { " cct, shanti, gyan prakash, new nepal"}; /* scl is a variable with array */
+
+
+        // 3.naming convention
+        // 1. naming convention of variable -----> use of camelCase( studentId, studentName....)
+        String librarySystem = " studentbooks.....";
+        String bankingSystem = " customerid.....";
+
+        int studentAge = 17;
+        double employeeId = 2.01d;
+        float numberOfBusCar = 5765.65645673f;
+
+        // 2. naming convention of the method /function ----> camelCase ----> e.g: numberAdd, numberSub,.....
+        // 3. naming convention of class ----> PascalCase -----> e.g: StudentClass, BankingSystem......
+
+
+        // 4. Types of datatype in java
+        int customerAge = 12;
+         double price = 744.7767;
+         float pointDigit = 657.786F;
+         char c = 'B';
+         boolean existComputer =  false;
+
+         long phoneNumber = 436579587895L;
+         short birthYear = 2009;
+         byte dateOfBirth = 10;
+
+
+
+         System.out.println("anshu");
     }
 }
